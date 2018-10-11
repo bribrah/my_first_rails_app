22a1f8dec8c2d52d24d5f191751b81e075cd6af3
bribrah <cheesemas46@gmail.com>
2018-10-10 22:53:34 -0700

    trying to fix bugs

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module MyFirstRailsApp
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
+    # -- all .rb files in that directo into files in config/initializers
+    # the framework and any gems in yory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 end
